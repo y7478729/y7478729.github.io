649de606e45b6e4ea10de86a1b58d77b2d5b85ca
y7478729 <7478729@qq.com>
2024-03-23 10:48:26 +0800

    new file:   js/setting.js

---
+++ js/setting.js
@@ -0,0 +1,7 @@
+//主题&广告链接
+var themeUrl = ["https://sub.tsutsu.cc","https://b.sub.tsutsu.cc","https://o.sub.tsutsu.cc"]; //建立数组储存链接
+var qfUrl = ["https://caofei.me","https://tsutsu.cc"]; //建立数组储存链接
+
+//主题信息设置
+var themeName = "鲸歌";
+var themeAuthor = "つつ";
